6d1e4c6ea0bbf7f2c0825acab3eaedc3811ad2d4
JustinBenning <justin.benning@gmail.com>
2024-01-31 12:35:10 -0500

    upload of exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameName = new List<string>() { "F123", "NASCAR Heat", "NHL2K19", "WWE2K23", "Call of Duty", "Halo" };
+
+            gameName.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
